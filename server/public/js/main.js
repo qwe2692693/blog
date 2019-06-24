02bca6d67b1c57dec283bf4fff8c4a317bcb3efd
Arash眼眸 <772717529@qq.com>
2019-06-24 17:22:26 +0800

    当前时间方法一般

---
+++ server/public/js/main.js
@@ -0,0 +1,12 @@
+// 获取当前的系时间
+let nowDate = '';
+
+getNowDat()
+//获取时间的方法
+function getNowDat() {
+    let dateObj = new Date(),
+        year = dateObj.getFullYear(),
+        
+
+}
+
--- server/routers/viewPage.js
+++ server/routers/viewPage.js
@@ -3,7 +3,8 @@ const router = express.Router()
 
 router.get('/main',(req,res)=>{
     res.render('main',{
-        title: 'main页面'
+        title: 'main页面',
+        username: req.session.user.nickname,
     })
 })
 module.exports = router;
