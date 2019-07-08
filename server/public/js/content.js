785df66014ffde5924e701ce09f129df88af66f9
772717529@qq.com <dvdvd>
2019-07-08 17:31:02 +0800

    增加按钮编辑删除

--- server/public/js/content.js
+++ server/public/js/content.js
@@ -3,9 +3,10 @@ layui.use(['table', 'jquery', 'layer'], function() {
         $ = layui.jquery,
         layer = layui.layer,
         index;
-    $(".addCon").on('click', function() {
+
+    function openPageFun(title) {
         index = layer.open({
-            title: '添加内容',
+            title: title,
             type: 2,
             content: 'contentAdd',
         });
@@ -18,23 +19,33 @@ layui.use(['table', 'jquery', 'layer'], function() {
         $(window).on('resize', function() {
             layer.full(index);
         })
+    }
+    $(".addCon").on('click', function() {
+        openPageFun('添加页面')
     })
     table.render({
         elem: '#contentTable',
         url: '/content',
+        height: "full-125",
         cols: [
             [ //表头
-                { checkbox: true },
+                { checkbox: true, fixed: 'left' },
                 { field: '_id', title: 'ID', sort: true, },
                 { field: 'title', title: '文章标题' },
                 { field: 'category', title: '所属栏目', templet: '#categoryTpl' },
                 { field: 'user', title: '发布者', templet: '#userTpl' },
-                { field: 'addTime', title: '发布时间' }
+                { field: 'addTime', title: '发布时间' },
+                { title: '操作', width: 110, templet: '#oper' }
             ]
         ],
         page: true, //开启分页
-
     })
-
-
+    table.on('tool(contentTable)', function(obj) {
+        let data = obj.data;
+        if (obj.event == 'edit') {
+            alert('编辑')
+        } else if (obj.event == 'delet') {
+            alert('删除')
+        }
+    })
 })
