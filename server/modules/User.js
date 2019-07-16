9272f61042d830dccdb0bb682a1f27ff3a41a4df
772717529@qq.com <dvdvd>
2019-07-16 16:31:46 +0800

    修改添加时间问题

--- server/modules/User.js
+++ server/modules/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const usersSchemas = require('../schemas/user')
 const date = require('../config/date');
 
-usersSchemas.pre('save', (next) => {
+usersSchemas.pre('save', function(next) {
     this.addTime = date();
     next()
 })
