0f6225c6efd6abf7b208d97c1b2d5d476855fbb0
772717529@qq.com <dvdvd>
2019-07-16 16:52:00 +0800

    新增管理员

--- server/public/js/userAdmin.js
+++ server/public/js/userAdmin.js
@@ -1,6 +1,7 @@
-layui.use(['jquery', 'table'], function() {
+layui.use(['jquery', 'table', 'form'], function() {
     let $ = layui.jquery,
-        table = layui.table;
+        table = layui.table,
+        form = layui.form;
     table.render({
         elem: '#table',
         url: '/user',
@@ -9,13 +10,16 @@ layui.use(['jquery', 'table'], function() {
                 { field: '_id', title: 'ID', sort: true, fixed: 'left' },
                 { field: 'nickname', title: '昵称' },
                 { field: 'username', title: '用户名' },
-                { field: 'addTime', title: '注册时间' }
+                { field: 'addTime', title: '注册时间' },
+                { field: 'isAdmin', title: '管理员', templet: '#userTpl', width: 85, }
             ]
         ],
         page: true
 
     })
-    $.get('/user', function(data) {
-        console.log(data)
-    })
+    form.on('switch(isAdminBtn)', function(obj) {
+        console.log(obj)
+            // layer.tips(this.value + ' ' + this.name + '：' + obj.elem.checked, obj.othis);
+    });
+
 })
