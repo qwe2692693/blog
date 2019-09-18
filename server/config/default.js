ec5110f020dc191f7bbf073d6a78677dc13ba8a7
木子 <oncwnuCftiRUZEsvtaimlY2QUlUg@git.weixin.qq.com>
2019-09-18 15:06:53 +0800

    修改存在时间

--- server/config/default.js
+++ server/config/default.js
@@ -1,15 +1,15 @@
 // 配置
 module.exports = {
-  port: process.env.PORT || 8082,
-  url: 'mongodb://localhost:27018/blog',
-  session: {
-    secret: 'SID',
-    name: 'SID',
-    resave: true,
-    saveUninitialized: false, // 是否保存未初始化的会话
-    cookie: {
-      secure: false,
-      maxAge: 365 * 24 * 60 * 60 * 1000
+    port: process.env.PORT || 8082,
+    url: 'mongodb://localhost:27018/blog',
+    session: {
+        secret: 'SID',
+        name: 'SID',
+        resave: true,
+        saveUninitialized: false, // 是否保存未初始化的会话
+        cookie: {
+            secure: false,
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        }
     }
-  }
 }
