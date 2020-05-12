d7df415757f88460951b03f5fbac72528d24296c
liyicheng123 <liyicheng>
2020-05-12 14:31:56 +0800

    修改上传

--- server/routers/upload.js
+++ server/routers/upload.js
@@ -2,41 +2,15 @@ const express = require('express')
 const router = express.Router()
 const formidable = require('formidable')
 const fs = require('fs');
-
-
-// router.post('/', (req, res) => {
-//     let form = new formidable.IncomingForm();
-//     // 创建临时目录
-//     let dir = form.uploadDir = "./public/upload/"
-//     form.parse(req, (err, fields, files) => {
-//         // console.log(files.file)
-//         // return
-//         if (err) {
-//             return console.log('formidable, form.parse err' + err);
-//         }
-//         //根据前端设置
-//         let filspath = files.myFileName.path
-//         let filsname = files.myFileName.name
-//         let newPath = dir + filsname
-//         fs.rename(filspath, newPath, (err) => {
-//             if (err) {
-//                 console.log("上传错误" + err)
-//                 return res.send({ isOk: false, err })
-//             }
-//             let resPath = newPath.replace("./public", "/static"); //处理图片路径  让前端能访问
-//             res.json({ isOk: true, data: [resPath], errno: 0 }) //返回图片路径
-//         })
-//     })
-// })
-
+let dir = "./public/upload";
+let date = new Date(),
+    year = date.getFullYear(),
+    month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1),
+    d = date.getDate() < 10 ? '0' + date.getDate() : date.getDate(),
+    dateStr = year + month + d;
+let dirDate = dir + '/' + dateStr;
 router.post('/', (req, res) => {
-    let dir = "./public/upload";
-    let date = new Date(),
-        year = date.getFullYear(),
-        month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1),
-        d = date.getDate() < 10 ? '0' + date.getDate() : date.getDate(),
-        dateStr = year + month + d;
-    let dirDate = dir + '/' + dateStr;
+
     fs.access(dir, fs.constants.F_OK, (err) => {
         if (err) {
             mkdirFun(dir)
@@ -74,12 +48,15 @@ router.post('/', (req, res) => {
 })
 
 function mkdirFun(obj) {
-    fs.mkdir(obj, (err) => {
+    fs.mkdirSync(obj, (err) => {
         if (err) {
             console.log(err)
         }
         console.log('创建成功')
     })
 }
+
+
+
 
 module.exports = router;
